84716c31354340478f439f1257e3f1973289da6a
Priscyllaamaral <57007327+Priscyllaamaral@users.noreply.github.com>
2023-04-14 16:21:06 -0300

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,5 +1,62 @@
 // arquivo chamado index.js dentro da pasta controller
 
+
+
+const modulo = require('../modulo');
+let posts = [];
+
+module.exports = {
+
+    post : function (req, res, next) {
+          posts.push(req.body);
+          res.status(201).send(posts);
+          console.log("post");
+    },
+
+    put : function (req, res, next) {
+          let usuario = posts.find(x => x.nome == req.params.nome);
+          usuario.nome = req.body.nome;
+          usuario.idade = req.body.idade;
+          res.status(201).send(usuario);
+    },
+
+    patch : function (req, res, next){
+          let usuario = posts.find(x => x.nome == req.params.nome);
+
+          usuario.nome = req.body.nome;
+          usuario.idade = req.body.idade;
+
+          res.status(201).send(usuario);
+    },
+
+    all : function(req,res, next) {
+          console.log('Accessing the secret section ...');
+          res.status(404).send('Seção secreta');
+    },
+
+    delete : function(req, res, next) {
+          posts = posts.filter(x => x.nome != req.params.nome);
+          res.status(201).send(posts);
+    },
+
+    get : function (req, res, next) {
+          res.status(200).send(posts);
+          console.log(modulo.area(10));
+    },
+      
+    postId : function(req, res, next){
+          res.status(201).send(`user ${req.params.id}`);
+  
+    }
+}
+
+
+
+//Outra forma de escrever o mesmo código
+
+
+/**
+
       const modulo = require('../modulo');
       let posts = [];
 
@@ -38,3 +95,5 @@
           res.status(201).send(`user ${req.params.id}`);
 
       }
+      
+      */

ef694204765878c356f92b96b13bf2425f1a0ca0
Priscyllaamaral <57007327+Priscyllaamaral@users.noreply.github.com>
2023-04-14 11:33:04 -0300

    Update and rename README.md to app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+# projetoexpress
+
+## npm init -y
+## npm i express body-parser
+## npm run server
+
+### utlizar o postman para testar as rotas
+
+
+
+    var express = require('express');
+    var bodyparser = require('body-parser');
+    var app = express();
+    var wiki = require('./router/index');
+
+
+    app.use(bodyparser.json());
+    app.use("/", wiki);
+
+    app.use(function (err, req, res, next) {
+        console.error(err.stack);
+        res.status(500).send("Something broke!");
+    });
+
+    var requestTime = function (req, res, next) {
+        req.requestTime = Date.now();
+        console.log(req.requestTime, Date.now());
+        next();
+      };
+
+    app.use(requestTime);
+    app.use('/media',express.static('public'));
+
+    app.listen(3000, function() {
+      console.log('App de Exemplo escutando na porta 3000!');
+    });
